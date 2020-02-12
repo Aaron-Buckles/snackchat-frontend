e0c8ad5ff7ceec20e2a25126de70fbcf83e8c2af
Aaron Buckles <abuck351@gmail.com>
2020-02-11 20:24:20 -0800

    Separate backend

--- backend/routes/tags.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const TagController = require("../controllers/tags-controller");
-
-router.get("/", TagController.getAllTags);
-router.post("/", TagController.createTag);
-
-module.exports = router;
