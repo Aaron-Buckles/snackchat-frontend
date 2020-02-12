8ca42ed0847f6668a17055ea0e35e279eb351876
Aaron Buckles <aaronjbuckles@outlook.com>
2020-02-11 20:14:19 -0800

    Build folder

---
+++ frontend/build/precache-manifest.30768606be28aceb4cfe27f98278900b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534cfddaf3993668983acec43628c2b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0337365cd6885ee0b6a",
+    "url": "/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "a695353f09c8aa4771a5",
+    "url": "/static/css/main.e99c63fb.chunk.css"
+  },
+  {
+    "revision": "a0337365cd6885ee0b6a",
+    "url": "/static/js/2.2d1bb2e2.chunk.js"
+  },
+  {
+    "revision": "0025667ad975d5b5bf18d67601a8b988",
+    "url": "/static/js/2.2d1bb2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a695353f09c8aa4771a5",
+    "url": "/static/js/main.4f75d36b.chunk.js"
+  },
+  {
+    "revision": "d7466656eda4cad01e8e",
+    "url": "/static/js/runtime-main.1bd1ec8e.js"
+  }
+]);
